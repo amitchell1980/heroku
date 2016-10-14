fe80f84dfdae24f2600eefb91c27ef1c505a748c
Archie Mitchell <amitchell1980@yahoo.com>
2016-10-14 04:26:10 -0700

    heroku files

--- lwc/settings/production.py
+++ lwc/settings/production.py
@@ -1,10 +1,24 @@
-from .base import *
 
-try:
-    from .local import *
-    live = False
-except:
-    live = True
+import os
+from django.conf import settings
 
-if live:
-    from .product import *
+DEBUG = False
+TEMPLATE_DEBUG = True
+DATABASES = settings.DATABASES
+
+import odj_database_url
+DATABASES = ['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+# import os
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+#
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
